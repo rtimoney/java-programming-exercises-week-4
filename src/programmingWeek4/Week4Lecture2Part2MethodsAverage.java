d46f0b0fbf7712435bbf04b64b7fba39c52119c8
rtimoney <103318517+rtimoney@users.noreply.github.com>
2023-02-19 19:14:43 +0000

    Update Week4Lecture2Part2MethodsAverage.java

--- src/programmingWeek4/Week4Lecture2Part2MethodsAverage.java
+++ src/programmingWeek4/Week4Lecture2Part2MethodsAverage.java
@@ -7,7 +7,7 @@ public class Week4Lecture2Part2MethodsAverage {
 		System.out.println("Start");
 		avg(1,2,3,4);
 		System.out.println("End");
-	}
+	}  // end of main method
 	
 	/**
 	 * This method calculates the average of our 4 ints
@@ -22,6 +22,6 @@ public class Week4Lecture2Part2MethodsAverage {
                                // have to write 4.0 so that the answer doesn't revert back to an int		
 		double avg = sum/4.0;
 		System.out.println("The average is : "+avg);
-	}
+	} // end of avg function
 
-}
+} // end of class
